82dd84a1bfb53177a84330a8991bb58bb41f6499
marshall-johnson <marshalljohnsonp@gmail.com>
2025-05-05 18:13:56 -0600

    link

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -34,13 +34,17 @@ const Login = ({
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        `https://mern-expense-tracker-t3dj.onrender.com/api/auth/login`,
+        {
+          // const response = await fetch(`http://localhost:5000/api/auth/login`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
 
       if (!response.ok) {
         setMessage("Login request failed");
